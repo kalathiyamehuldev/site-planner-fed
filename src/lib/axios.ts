ee7444c45967ac5f229a9ef6e19d3cd2214e6c7e
kalathiya.mehul <140096665+kalathiyamehul@users.noreply.github.com>
2025-09-22 14:22:55 +0530

    Update axios.ts

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { toast } from 'sonner';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://87.201.193.142:8000';
 
 // Create axios instance
 const api = axios.create({
